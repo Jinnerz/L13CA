69ca0d6e8e8b5e34cde0e6054b7e0d4363b36466
Jinnerz <chuayongjin@gmail.com>
2025-02-11 20:03:11 +0800

    Sound Commit

--- Home.js
+++ Home.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, StatusBar, FlatList, TextInput, TouchableOpacity} from 'react-native';
+import soundfile from "./short1.wav";
+import {Sound} from "expo-av/build/Audio/Sound";
+import { Audio } from 'expo-av';
 
+//sound - npx expo install expo-av
 
 const styles = StyleSheet.create({
     TitleBox: {
@@ -12,6 +16,27 @@ const styles = StyleSheet.create({
 const Home = ({navigation}) => {
     const [myData, setMyData] = useState([]);
     const [originalData, setOriginalData] = useState([]);
+    const [mySound, setMySound] = useState();
+
+    //to help prevent issues from arising if user spams the button, unloading the sound before the next plays
+    async function playSound() {
+        if (mySound) {
+            await mySound.unloadAsync();
+        }
+        const { sound } = await Audio.Sound.createAsync(require('./short1.wav'));
+        setMySound(sound);
+        await sound.playAsync();
+    }
+
+
+    useEffect(() => {
+        return mySound
+            ? () => {
+                console.log('Unloading Sound');
+                mySound.unloadAsync();
+            }
+            :undefined
+    }, [mySound]);
 
     useEffect(() => {
         fetch ("https://data.gov.sg/api/action/datastore_search?resource_id=d_688b934f82c1059ed0a6993d2a829089")
@@ -43,6 +68,7 @@ const Home = ({navigation}) => {
                 onPress={()=>
                 {
                     navigation.navigate("Details",{name: item.school_name, url: item.url_address, postal: item.postal_code, email: item.email_address, phone: item.telephone_no, principal: item.principal_name, viceprincipal: item.first_vp_name})
+                    playSound();
                 }
                 }
             >
